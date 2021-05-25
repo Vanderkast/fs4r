8184ff0b222ec5987ee771a6af55e54f199ce96d
Vanderkast <vanderkast@yandex.ru>
2021-05-25 18:57:04 +0300

    upd

--- service/src/main/java/net/vanderkast/fs4r/service/configuration/SecurityConfiguration.java
+++ service/src/main/java/net/vanderkast/fs4r/service/configuration/SecurityConfiguration.java
@@ -1,13 +1,16 @@
 package net.vanderkast.fs4r.service.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import static net.vanderkast.fs4r.service.rest.MainRestController.API_PATH;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -16,23 +19,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private static final String AUTHORITY_WRITE = "WRITE";
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors()
+    protected void configure(HttpSecurity security) throws Exception {
+        security.cors()
                 .and()
                 .authorizeRequests()
-                .antMatchers(API_PATH + "/walk/**", API_PATH + "load/**", API_PATH + "/download/**")
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers("/api/v1/main/walk/**", "/api/v1/main/load/**", "/api/v1/main/download/**")
                 .hasAuthority(AUTHORITY_READ)
 
-                .and()
-                .authorizeRequests()
-                .antMatchers(API_PATH + "/move/**", API_PATH + "/delete/**", API_PATH + "/upload/**")
-                .hasAuthority(AUTHORITY_WRITE)
-
-                .and().httpBasic();
+                .antMatchers("/api/v1/main/move/**", "/api/v1/main/delete/**", "/api/v1/main/upload/**", "api/v1/lock")
+                .hasAuthority(AUTHORITY_WRITE);
+        security.httpBasic();
+        security.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { // todo pass passwords via properties
+    public void users(AuthenticationManagerBuilder auth) throws Exception { // todo pass passwords via properties
         auth.inMemoryAuthentication()
                 .withUser("regular")
                 .password("{bcrypt}$2y$12$xznUZSKJX3t1Hy1iY5T6GeOL9iGcVxeiFCokOs.tFPdarSImsaMKS")
---
+++ service/src/main/java/net/vanderkast/fs4r/service/service/StampLockService.java
@@ -0,0 +1,6 @@
+package net.vanderkast.fs4r.service.service;
+
+import java.util.UUID;
+
+public interface StampLockService {
+}
