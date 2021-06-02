5e30c4ea9a73f71ee329626dffd03061a66151eb
Vanderkast <vanderkast@yandex.ru>
2021-06-02 14:07:36 +0300

    1.0-RELEASE

--- core/src/main/java/net/vanderkast/fs4r/dto/WriteDto.java
+++ core/src/main/java/net/vanderkast/fs4r/dto/WriteDto.java
@@ -9,12 +9,13 @@ public interface WriteDto {
     InputStream getInputStream();
 
     /**
-     * Affects only when {@link WriteDto#isReplace()} is false.
-     * @return true - overwrite exist data, false - append to exist data
+     * Affects only when {@link WriteDto#isReplace()} is false or target file doesn't exists.
+     * @return true - overwrite existing file content, false - append to it
      */
     boolean isOverwrite();
 
     /**
+     * If false means <i>"fail on target file exists already"</i>.
      * @return true if replaces existing file, false - if should fail.
      */
     boolean isReplace();
