23a8a90d537fe197043ef34d04e0eb8dcc176b39
Vanderkast <vanderkast@yandex.ru>
2021-06-02 12:41:12 +0300

    1.0-RELEASE

--- core/src/main/java/net/vanderkast/fs4r/simple/JustWrite.java
+++ core/src/main/java/net/vanderkast/fs4r/simple/JustWrite.java
@@ -6,19 +6,19 @@ import net.vanderkast.fs4r.dto.WriteDto;
 import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.StandardOpenOption;
 
 public class JustWrite implements Write {
     @Override
     public void write(WriteDto data) throws IOException {
-        if(data.isReplace() && Files.exists(data.getPath()))
+        if (!data.isReplace() && Files.exists(data.getPath()))
             throw new FileAlreadyExistsException(data.getPath().toString());
         if (data.isOverwrite()) {
             data.getInputStream().transferTo(Files.newOutputStream(data.getPath()));
         } else {
             data.getInputStream()
-                    .transferTo(Files.newOutputStream(data.getPath(), StandardOpenOption.APPEND));
+                    .transferTo(Files.newOutputStream(data.getPath(),
+                            StandardOpenOption.CREATE, StandardOpenOption.APPEND));
         }
     }
 }
